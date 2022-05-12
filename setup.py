6ad07337dddc982ecf54de76c5c63874dccedd24
Jason Carver <ut96caarrs@snkmail.com>
2022-05-12 10:32:03 -0700

    Downgrade Jinja2 and MarkupSafe for compatibility
    When trying to make docs, was getting an
      ImportError: cannot import name 'contextfunction' from 'jinja2'

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ extras_require = {
         "Sphinx>=1.6.5,<2",
         "sphinx_rtd_theme>=0.1.9,<1",
         "towncrier>=19.2.0, <20",
+        "Jinja2<3",
+        "MarkupSafe<2",
     ],
     'dev': [
         "bumpversion>=0.5.3,<1",
