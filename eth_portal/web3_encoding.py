c03bdd272463543310db3ba7c80e28f2d1453b44
Jason Carver <ut96caarrs@snkmail.com>
2022-05-17 13:49:07 -0700

    Method to encode header content ID, plus test

---
+++ eth_portal/web3_encoding.py
@@ -0,0 +1,31 @@
+import ssz
+from ssz.sedes import (
+    Container,
+    Vector,
+    uint8,
+    uint16,
+)
+
+#
+# SSZ encoding
+#
+
+HEADER_TYPE_BYTE = b'\x01'
+HEADER_SEDES = Container((
+    uint16,  # Chain ID
+    Vector(uint8, 32),  # header hash
+))
+
+
+def header_content_id(header_hash, chain_id=1):
+    """
+    Convert a header hash into a content ID for the Portal History Network.
+
+    Include the chain ID, which defaults to mainnet.
+    """
+    # The header type ID is implicitly defined in the SSZ union which
+    # specifies the content ID on the header history network.
+    encoded = ssz.encode((chain_id, header_hash), HEADER_SEDES)
+
+    # I don't think py-ssz supports Union types, so manually tacking it on
+    return HEADER_TYPE_BYTE + encoded
