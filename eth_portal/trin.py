c96713514a2371699c800e6fa7ac3aff5754346f
Jason Carver <ut96caarrs@snkmail.com>
2022-05-18 12:39:27 -0700

    Read Portal keys out of environment variable

--- eth_portal/trin.py
+++ eth_portal/trin.py
@@ -9,6 +9,7 @@ from eth_keys.datatypes import (
 )
 from eth_utils import (
     keccak,
+    remove_0x_prefix,
 )
 from web3 import Web3
 
@@ -30,6 +31,7 @@ def launch_trin(private_key: bytes, port: int):
     public_key = PrivateKey(private_key).public_key.to_bytes()
     node_id_hex = keccak(public_key).hex()
     ipc_path = f"/tmp/trin-jsonrpc-{node_id_hex[:20]}.ipc"
+    private_key_hex = remove_0x_prefix(private_key.hex())
 
     print(f"Launching trin with node ID {node_id_hex[:10]}...")
     try:
@@ -37,7 +39,7 @@ def launch_trin(private_key: bytes, port: int):
             [
                 "./trin",
                 "--discovery-port", str(port),
-                "--unsafe-private-key", private_key.hex()[2:],
+                "--unsafe-private-key", private_key_hex,
                 "--web3-ipc-path", ipc_path,
                 "--kb", "20000",
                 "--networks", "history",
--- scripts/launch_bridge.py
+++ scripts/launch_bridge.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
+import os
+import sys
 import time
+
+from eth_utils import decode_hex
 
 from eth_portal.bridge import (
     handle_new_header,
     launch_trin_inserters,
+)
+
+
+INVALID_KEY_ENV_ERROR = (
+    "Must supply environment variable PORTAL_BRIDGE_KEYS, as a"
+    " comma-separated list of hex-encoded Portal client 32-byte private keys"
 )
 
 
@@ -15,13 +25,9 @@ def header_log_loop(w3, portal_inserter, event_filter, poll_interval):
 
 
 def launch_bridge():
-    #TODO Load private keys for launching trin nodes from environment
-    trin_node_keys = [
-        b'rainbowsunicornsrainbowsunicorns',
-    ]
-
     # Launch trin nodes, for broadcasting data
     # The context manager shuts down all trin nodes on context exit
+    trin_node_keys = load_private_keys()
     with launch_trin_inserters(trin_node_keys) as portal_inserter:
         # Monitor for new headers on mainnet
         from web3.auto.infura import w3
@@ -29,6 +35,20 @@ def launch_bridge():
 
         # On each new header, publish the content to the trin nodes
         header_log_loop(w3, portal_inserter, block_filter, 6)
+
+
+def load_private_keys():
+    try:
+        concat_keys = os.environ['PORTAL_BRIDGE_KEYS']
+    except KeyError:
+        sys.exit(INVALID_KEY_ENV_ERROR)
+    else:
+        hex_keys = concat_keys.split(',')
+        keys = [decode_hex(key) for key in hex_keys]
+        if any(len(key) != 32 for key in keys):
+            sys.exit(INVALID_KEY_ENV_ERROR)
+        else:
+            return keys
 
 
 if __name__ == '__main__':
