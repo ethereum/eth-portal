0d36f2fe9fd55478aee8bbcfbad8f598c5572965
Jason Carver <ut96caarrs@snkmail.com>
2022-05-20 13:10:47 -0700

    Encode receipt content key

--- tests/core/test_web3_encoding.py
+++ tests/core/test_web3_encoding.py
@@ -5,6 +5,7 @@ import pytest
 
 from eth_portal.web3_encoding import (
     header_content_key,
+    receipt_content_key,
 )
 
 
@@ -21,6 +22,24 @@ from eth_portal.web3_encoding import (
         ),
     )
 )
-def test_encode_header(chain_id, header_hash, expected_encoding):
+def test_encode_header_key(chain_id, header_hash, expected_encoding):
     content_key = header_content_key(header_hash, chain_id)
+    assert content_key == expected_encoding
+
+
+@pytest.mark.parametrize(
+    'chain_id, header_hash, expected_encoding',
+    (
+        (2, b'H' * 32, b'\x02\x02\x00' + b'H' * 32),
+        (
+            # Pulled test data from:
+            # https://github.com/ethereum/portal-network-specs/blob/2fb0116314c407feff4ce678dac2da5adea834eb/content-keys-test-vectors.md#headerkey
+            4,
+            decode_hex('0xd1c390624d3bd4e409a61a858e5dcc5517729a9170d014a6c96530d64dd8621d'),
+            decode_hex('0x020400d1c390624d3bd4e409a61a858e5dcc5517729a9170d014a6c96530d64dd8621d'),
+        ),
+    )
+)
+def test_encode_receipt_key(chain_id, header_hash, expected_encoding):
+    content_key = receipt_content_key(header_hash, chain_id)
     assert content_key == expected_encoding
