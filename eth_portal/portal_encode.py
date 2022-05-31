f2b38f5cb214cee921219cfb6fbf63a9c582fb5c
Jason Carver <ut96caarrs@snkmail.com>
2022-05-31 14:48:09 -0700

    Generate content value for block bodies
    Validate against the uncles and transactions root on every generation.
    Add tests for value generation. (no reference value though)

--- eth_portal/portal_encode.py
+++ eth_portal/portal_encode.py
@@ -1,3 +1,4 @@
+import rlp
 import ssz
 from ssz.sedes import Byte, Container, List, Vector, uint8, uint16
 
@@ -15,6 +16,18 @@ BLOCK_KEY_SEDES = Container(
         Vector(uint8, 32),  # header hash
     )
 )
+
+TRANSACTIONS_SEDES = List(
+    # Each encoded transaction
+    List(Byte(), 65535),  # TODO identify true upper-bound on encoded transaction length
+    65535,  # 2**16-1 transaction would use up >1.3 billion gas at 21k gas each
+)
+UNCLES_SEDES = List(
+    # Each encoded header
+    List(Byte(), 65535),  # TODO identify true upper-bound on encoded header length
+    255,  # Maximum number allowed currently is 2. 2**8-1 leaves some room for expansion
+)
+BLOCK_BODY_SEDES = Container((TRANSACTIONS_SEDES, UNCLES_SEDES))
 
 BLOCK_RECEIPTS_SEDES = List(
     # Each encoded receipt
@@ -46,6 +59,18 @@ def block_body_content_key(header_hash, chain_id=1):
     # See implementation notes in header_content_key()
     encoded = ssz.encode((chain_id, header_hash), BLOCK_KEY_SEDES)
     return BODY_TYPE_BYTE + encoded
+
+
+def block_body_content_value(transactions, uncles):
+    """
+    Compile a list of transactions and uncle headers into a block body content value.
+    """
+    # Awkward quirk that ssz must take an iterable of individual bytes, instead of `bytes`
+    encoded_transactions = [
+        [bytes([byte]) for byte in transaction.encode()] for transaction in transactions
+    ]
+    encoded_uncles = [[bytes([byte]) for byte in rlp.encode(uncle)] for uncle in uncles]
+    return ssz.encode((encoded_transactions, encoded_uncles), BLOCK_BODY_SEDES)
 
 
 def receipt_content_key(header_hash, chain_id=1):
